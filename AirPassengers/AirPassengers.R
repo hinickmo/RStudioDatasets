a2694b526847b1c59f35d74be9ed356c7776543a
Nick <ncsu.nickmo@gmail.com>
2022-07-30 11:14:32 -0400

    new

---
+++ AirPassengers/AirPassengers.R
@@ -0,0 +1,37 @@
+library(ggplot2) #qplot (basic scatterplot) 
+library(ggthemes)
+library(tidyverse)
+library(datasets)
+library(ggfortify)
+install.packages("forecast") #for autoplot with geom_smooth linear regression
+install.packages("tseries") #for autoplot with geom_smooth linear regression
+?datasets
+library(help = "datasets")
+?AirPassengers
+
+AirPassengers
+head(AirPassengers)
+#this dataset does not have column names, but that won't stop us
+
+class(AirPassengers) #ts is the output = this means the data is a time series 
+start(AirPassengers) #shows first year 
+end(AirPassengers) #shows last year
+
+plot(AirPassengers, xlab="Year", ylab="Air Passengers (1,000s)", main="Time Series of Air Passengers", 
+     sub="The classic Box & Jenkins airline data. Monthly totals of international airline passengers, 1949 to 1960.")
+
+
+boxplot(AirPassengers~cycle(AirPassengers),xlab="Date", ylab = "Air Passengers (1,000s)" ,main ="Monthly Air Passengers Boxplot from 1949 to 1961")
+
+autoplot(AirPassengers) + 
+  geom_smooth(method="lm")+ 
+  labs(x ="Date", y = "Air Passengers (1,000s)", title="Air Passengers from 1949 to 1961") 
+  
+
+arimaAirPassengers <- auto.arima(AirPassengers)
+arimaAirPassengers
+
+ggtsdiag(arimaAirPassengers)
+
+forecastAirPassengers <- forecast(arimaAirPassengers, level = c(95), h = 48)
+autoplot(forecastAirPassengers)
