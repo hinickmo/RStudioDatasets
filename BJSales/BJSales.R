4be2776e404f7bf66097e84195391eb2af946e97
Nick <ncsu.nickmo@gmail.com>
2022-07-31 21:43:25 -0400

    new

--- BJSales/BJSales.R
+++ BJSales/BJSales.R
@@ -19,3 +19,24 @@ end(BJsales) #shows last year
 plot(BJsales, xlab="Time", ylab="Sales", main="Sales ", col.main="SteelBlue",
      sub="The data are given in Box & Jenkins (1976). Obtained from the Time Series Data Library at https://robjhyndman.com/TSDL/")
 
+boxplot(BJsales~cycle(BJsales),xlab="Date (unknown scale)", ylab = "Sales (unknown scale)" ,main ="Sales Over Time")
+
+autoplot(BJsales) + 
+  geom_smooth(method="lm")+ 
+  labs(x ="Date (unknown scale)", y = "Sales (unknown scale)", title="Sales Over Time") 
+
+#ARIMA is an acronym for “autoregressive integrated moving average.” It's a model used in statistics and econometrics to measure events that happen over a period of time.
+arimaBJsales <- auto.arima(BJsales)
+arimaBJsales
+
+#if the residuals plot is around - w/ no movement, then ARIMA model is a good fit
+ggtsdiag(arimaBJsales)
+
+#95% confidence looking 48 months into the future
+forecastBJsales <- forecast(arimaBJsales, level = c(95), h = 10)
+autoplot(forecastBJsales)
+
+
+
+forecastedJsales <- forecast(arimaBJsales, 10)
+plot(forecastedJsales, main="Forecast of Sales Over Time", col.main="SteelBlue", ylab = "Sales (unknown scale)", xlab="Date (unknown scale)")
