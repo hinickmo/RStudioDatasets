137a4324961270afdeec1d8edc49474cd9844976
Nick <ncsu.nickmo@gmail.com>
2022-08-05 20:10:56 -0400

    new

--- EuStockMarkets/EUStockMarkets.R
+++ EuStockMarkets/EUStockMarkets.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(datasets)
 library(ggfortify)
 library(rmarkdown)
+    library(broom)
+    library(dplyr)
 
 ?datasets
 library(help = "datasets")
@@ -31,6 +33,18 @@ ggplot(EuStockDF,aes(x = Date))+
   geom_line(aes(y = DAX), color="darkslateblue")+
   geom_line(aes(y = SMI), color="darkorchid")+
   geom_line(aes(y = CAC), color="mediumseagreen")+
-  geom_line(aes(y = FTSE), color="indianred3")
+  geom_line(aes(y = FTSE), color="indianred3")+  
+  theme(legend.position = "bottom")
 
+tidyEuStockMarkets <- tidy(EuStockMarkets)
+tidyEuStockMarkets <- tidyEuStockMarkets %>%
+  rename(Date=index,Stock_Index=series,Price=value)
 
+tidyEuStockMarkets
+
+ggplot(tidyEuStockMarkets,aes(x=Date,y=Price))+
+  geom_line(aes(color=Stock_Index))+
+  expand_limits(y=c(2000, 10000))+
+  labs(title="European Stock Indices 1991 to 1998", x = "Year", y = "Stock Price",  
+       caption="Contains the daily closing prices of major European stock indices: Germany DAX (Ibis), Switzerland SMI, France CAC, 
+                and UK FTSE. The data are sampled in business time, i.e., weekends and holidays are omitted.")
