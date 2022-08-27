8655d9d614be1884ea4229af2836c70435fbd2d8
Nick <ncsu.nickmo@gmail.com>
2022-08-26 23:01:56 -0400

    new

--- LakeHuron/LakeHuron.R
+++ LakeHuron/LakeHuron.R
@@ -32,39 +32,64 @@ acf(LakeHuron)
 
 ts.plot(filter(LakeHuron,rep(1,17),method="convolution"))  
 
-
-
+#gradual decrease as the lags increase (this signifies a trend in the time series)
+ggAcf(LakeHuron)
+ggPacf(LakeHuron)
 
 
 # forecasting model using arima model Autoregressive Integrated Moving Average Model
-fit <- auto.arima(LakeHuron)
-fit2 <- arima(LakeHuron, order=c(1,0,1))
+#the mean value appears to be follow a pattern downward, so we can't use a simple ARMA model.
+fit <- auto.arima(LakeHuron) #(0,1,0)
+fit2 <- arima(LakeHuron, order=c(1,1,1))
+fit3 <- arima(LakeHuron, order=c(0,1,5))
+
+#AICc: The information score of the model (the lower-case ‘c’ indicates that the value has been calculated from the 
+#AIC test corrected for small sample sizes). The smaller the AIC value, the better the model fit.
+fit
+fit2 
+fit3
 
 # Next 5 forecasted values
 forecast(fit, 12)
 forecast(fit2, 12)
+forecast(fit3, 12)
+
 
 tsdiag(fit)
 tsdiag(fit2)
+tsdiag(fit3)
+
 
 #if the residuals plot is around - w/ no movement, then ARIMA model is a good fit
 ggtsdiag(fit)
 ggtsdiag(fit2)
+ggtsdiag(fit3)
 
 Box.test(fit$residuals,lag=1)
 Box.test(fit2$residuals,lag=1)
+Box.test(fit3$residuals,lag=1)
 
-
-
-LH.pred<-predict(fit2,n.ahead=8)
-
+LH.pred<-predict(fit,n.ahead=6)
+LH.pred2<-predict(fit2,n.ahead=6)
+LH.pred3<-predict(fit3,n.ahead=6)
 
 plot(LakeHuron,xlim=c(1875,1980),ylim=c(575,584)) 
 
-LH.pred<-predict(fit2,n.ahead=8) 
+LH.pred<-predict(fit,n.ahead=6) 
 lines(LH.pred$pred,col="red") 
 lines(LH.pred$pred+2*LH.pred$se,col="red",lty=3) 
 lines(LH.pred$pred-2*LH.pred$se,col="red",lty=3)
+
+LH.pred2<-predict(fit2,n.ahead=6) 
+lines(LH.pred2$pred,col="red") 
+lines(LH.pred2$pred+2*LH.pred$se,col="red",lty=3) 
+lines(LH.pred2$pred-2*LH.pred$se,col="red",lty=3)
+
+LH.pred3<-predict(fit3,n.ahead=6) 
+lines(LH.pred3$pred,col="red") 
+lines(LH.pred3$pred+2*LH.pred$se,col="red",lty=3) 
+lines(LH.pred3$pred-2*LH.pred$se,col="red",lty=3)
+
 
 plot(LH.pred, ylab = 'Water Level (Feet)', main = 'Lake Huron Annual Water Level 1875-1972')
 
@@ -82,7 +107,7 @@ plot(LH.pred, ylab = 'Water Level (Feet)', main = 'Lake Huron Annual Water Level
 plot(decompose(log(LakeHuron)))
 
 
-#Gamma = season / Beta = trend. False for gamme since this is annual data, not quarterly or monthly.
+#Gamma = season / Beta = trend. False for gamma since this is annual data, not quarterly or monthly.
 HWLakeHuron = HoltWinters(LakeHuron, gamma=FALSE, beta=TRUE)
 HWLakeHuron
 
